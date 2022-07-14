1e8bfcbcb7964f0394dee58170c76d4cf3020976
gdpnei2002 <joseneilson25@gmail.com>
2022-07-13 23:10:56 -0300

    LSP

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,13 +1,12 @@
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 interface IRequest{
     name: string;
     description: string;
 }
 
 class CreateCategoryService{
-    constructor(private categoriesRepository: CategoriesRepository) {}
-
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+    
     execute({description, name}: IRequest): void {
      const categoryAlreadyExists = this.categoriesRepository.findByName (name);
 
